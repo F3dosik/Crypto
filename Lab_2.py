13be45bb9cdd9dc7e5766324359291576e66a259
F3dosik <izhevsk2073@gmail.com>
2025-05-31 02:42:10 +0400

    Попытка #1

--- Lab_2.py
+++ Lab_2.py
@@ -1,10 +1,114 @@
-import numpy as np
 
-from Statistic import load_json
+from collections import Counter
+import math
+
+from Statistic import load_json, keep_russian_letters, load_text
 from pathlib import Path
-# Приближенное значение индекса совпадений для русского языка
+from math import gcd
+from functools import reduce
+
 # data = load_json(Path('statistic_symbols.json'))['stats']
-# index_of_matches = 0
-# for k in data:
-#     index_of_matches = data[k][]
-# print(data)
+# ind = 0
+# for symbol in data.values():
+#     ind += (symbol['percent']) ** 2
+# print(ind)
+rus = [' ', 'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
+       'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+
+
+#
+def vigenere(plaintext, key, mode=1):
+    plaintext = keep_russian_letters(plaintext)
+    key = keep_russian_letters(key)
+    res = ''
+    key_ind = 0
+
+    if mode:  # Шифрование
+        shift_func = lambda p, k: (rus.index(p) + rus.index(k)) % 33
+    else:  # Дешифрование
+        shift_func = lambda p, k: (rus.index(p) - rus.index(k)) % 33
+
+    for i in range(len(plaintext)):
+        p = plaintext[i]
+        k = key[key_ind]
+        res += rus[shift_func(p, k)]
+        key_ind = (key_ind + 1) % len(key)
+
+    return res
+
+
+# def find_repeats(text):
+#     distances = set()
+#     seen = set()
+#     for length in range(3, 4):
+#         for i in range(len(text) - length + 1):
+#             subtext = text[i:i + length]
+#             if subtext in seen:
+#                 continue
+#             else:
+#                 seen.add(subtext)
+#             start = i + length
+#             pre_pos = i
+#             while True:
+#                 pos = text.find(subtext, start)
+#                 if pos == -1:  # если больше не найдено
+#                     break
+#                 distances.add(pos - pre_pos)
+#                 start = pos + length  # сдвигаем стартовую позицию
+#                 pre_pos = pos
+#     return distances
+
+def find_repeats(text):
+    distances = set()
+    seen = set()
+    for length in range(3, 4):
+        for i in range(len(text) - length + 1):
+            subtext = text[i:i + length]
+            if subtext in seen:
+                continue
+            seen.add(subtext)
+            positions = []
+            start = 0
+            while True:
+                pos = text.find(subtext, start)
+                if pos == -1:
+                    break
+                positions.append(pos)
+                start = pos + 1
+            # Теперь для каждой подстроки считаем расстояния между соседними позициями
+            for j in range(1, len(positions)):
+                distances.add(positions[j] - positions[j-1])
+    return distances
+
+
+def get_common_divisor(distances):
+    divisors = []
+    for d in distances:
+        if d < 2:
+            continue
+        # Собираем все делители d
+        for i in range(2, int(math.sqrt(d)) + 1):
+            if d % i == 0:
+                divisors.append(i)
+                if i != d // i:
+                    divisors.append(d // i)
+    if not divisors:
+        return None
+    # Возвращаем самый частый делитель
+    return Counter(divisors).most_common(1)[0][0]
+
+
+plaintext = """Он до того струсил, увидав Шатова, что тотчас же захлопнул форточку и убежал на кровать. Шатов стал неистово стучать и кричать.
+   - Как вы смеете так стучать среди ночи? - грозно, но замирая от страху, крикнул Лямшин, по крайней мере минуты через две решившись отворить снова форточку и убедившись, наконец, что Шатов пришел один.
+   - Вот вам револьвер; берите обратно, давайте пятнадцать рублей.
+   - Что это, вы пьяны? Это разбой; я только простужусь. Постойте, я сейчас плед накину.
+   - Сейчас давайте пятнадцать рублей. Если не дадите, буду стучать и кричать до зари; я у вас раму выбью.
+   - А я закричу караул, и вас в каталажку возьмут.
+   - А я немой, что ли? Я не закричу караул? Кому бояться караула, вам или мне?"""
+p = """СЪСШ ЩГЖИСЮБЩЫРО ФЧ РЛЫОУУПЦЛЫ ЦЙУБЭЫФСЮДЯ ЛКЧААЮЦЩДХИЯ Б ХЙЕУЖ ШЩ ЧЙХК ЯПУЩА УОРЧЙ ЧЬЩ ЬЙЬЩУЙЙЧ Е ПЛЖЮС ЧАХОИ ЩЦ ЛЩДФСНБЮСЛ Щ ЙККЦЖЦЛЩ ЭЙСНШТ ЩЧЫОВХЮДИ ЗЗН ЛЪЯД ЛЕЖОН ЕЮЧЪЛМСРТЖЦЬВЖ ЛГСЗЙЬЧШ НФЧЗ ЧЮАЮЕ ЛЖЙКУАХЙНАИЕЬВ ЙЦЛ ККФЩУЮИЙЧ З ЬЦСЙВГЫХ СОЗЖЪНШШО ЛЪЯД ЦСЗНКЕШЛГЫХ ЦЩЗШО ЦСПЛЛТП С ЧАХЙВЩ ЮЙЦСЗХФС КЗСАХЦЩ СЙФФЗШО ЛЪЯД РЛЬНГЫХЪЖ ДПХЛЕЗ НФЧГХЛ ШЙ ШУЩ ЮОЕЛХЧУЛУ ЩКЯЙЛЩНКЫЭА ЕЧРЮЗЫГЧЖФЖ ЩЦ ЧРШЙЛЩМ ДЛВОЖЫРО КЙЯЛЫОЖЧЖФПШЙЪНХ ХЙЕЩЖ СЪСШ СЬЛРНГ ШПРТЗПЗН ЧЕЧУЦЖЪЕЩУС РЫСОНШЙ ЩЩТЖЛТЕЗ СЪСПХЛ СПРЬЛЕСЧШЙЪНХЩ ЪЙУЖЫЬЛ ЯЧВАЕЧИ ЩРЩТ ОЕФЖЫХЪЖ ДХЩЩЩХОВХЮДФ ЩРЩТ Щ ЗМУВ ЫЩГЕПЫЛЖПЯЛЩ Е ШУБЭЫЛЯЖ ЛЩДФСНБЮСЖ ШПБВЩ КЛЩА УОРЧЙ С ЛЪЯД Р ЮЯЙЭЩИЙЯЩ ЭЧНЛЯДФ ДЙРЧБЩЫРО ЫФЖ НЖЫФМ ЕРУЛКФТЕЗ У ЬЩУ ЧНШЙЪЖЧКИ ЧЩЫЙЕЧЗАФДЭСФ ЮЙНЭЩСЦТА З СЪСШ РГФПЛТ З ЙЪЬЛЕО ЛР ИОСЩХ АФЧЭЧ ЩЮЯОЧАИОЬШЙО ЦСЙМУБУХЬЛЖ ЪЩНЖЩСБЮСФ НЗНГЯХСЮАКУЛА ЬЙЧБМС Л ГЖФФШПШУБЕФФШЮЧФ ЛЪЬЮАЮСФ НИИ ДЛЯЧЫЛ ЙЩЪБЮСОЛЕЙЬШЙТ СЩЬЦЛ НЖЫФМ Е НФЧКУЩЕ КЙЧК ЮОЩФЦЧЧЩУЧ УБЬЦЩЛЪЩГЖЗО ЛЪЯ ЫГЯ ЭЙЕ ЧЙФПЯЙ ШУЩ ОЫЛР АЪВЛЕСЖР ЪЬЧАХ ЧААКШФЦЖЦГ НЖЫЖЕ ЕЧОЕЙПЬЛКЫП ЩЮЫФСЖЪЬЛТ С РЛЫОУУПЫФТГЦЩМ ЫОЖЧЖФПШЙЪНЩ УЦЩЪЙЧАСПРЛА ХСЦЛЕ ЛЛНЙЛ ЗЛЯХ ЛЪЯ ЦФЩЬКФУЮЧ ЕБЭ ЦФЩЬКФУЮЧ ЯШЙМЩЛЪЩГЖЗО СЩЬЦЛ ЯЙЫЩСАЗ ЩШЗ ЧНСППГЫХ УГЯ ЮОЛЖЪОСШЙ ХЬЛРЧЩФЯЙОЩЖ ЦФДУЧНСД ЦГ ЗЮОЫШЩЗ РРЙПФДХЕ ЛЪЯ ЧЧШЙМЩ ЧЗШГ ЕЙНФТЗ"""
+ciphertext = vigenere(plaintext, "КЛЮЧ")
+f = find_repeats(p)
+print(f)
+# aaaa
+# print(vigenere("привет", "ключ", 1))
+# print(vigenere("ъьжърю", "ключ", 0))
